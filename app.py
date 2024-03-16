724b7e7c9fe6f51aea7968d2ed9050d3fe263454
Affan Saiyed <affanimam123@gmail.com>
2024-03-16 18:08:27 -0400

    created user class, added field for company name

--- app.py
+++ app.py
@@ -7,17 +7,29 @@ from wtforms import FileField, SubmitField
 
 app = Flask(__name__)
 
+class User:
+     def __init__(self, job, description, experience, company):
+          self.job = job
+          self.description = description
+          self.experience = experience
+          self.company = company
+          return
+     
+     def __str__(self):
+          return f'Job Title: {self.job}, Preferred Qualifications: {self.description}, Hiring Company: {self.company}, Past Work Experience: {self.experience}>'
+
 @app.route('/')
 def home():
      return render_template('index.html')
 
 @app.route('/submit', methods = ['POST'])
-def input_form():
+def index():
       if request.method == 'POST':
+          comp_name = request.form['comp_name']
           job_title = request.form['job_title']
           job_desc = request.form['job_desc']
           work_exp = request.form['work_exp']
-      return render_template('index.html', title = job_title, desc = job_desc, exp = work_exp)
+      return render_template('index.html', comp = comp_name, title = job_title, desc = job_desc, exp = work_exp)
 
 if __name__ == "__main__":
     app.run()
